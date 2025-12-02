99ebf99950c3d6271578a59acdd17b23956bc902
Erik Alves <silvalveserik1@gmail.com>
2025-12-01 23:22:43 -0300

    Pequenos ajustes visuais ao botão 'ver todos os encontros' e ao footer

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -22,45 +22,47 @@ export default function BaseLayout({ children }: Props) {
       </main>
 
       <footer>
-        <p>Conteúdo baseado no material oficial da "Novena de Natal 2025" da Diocese de Campo Limpo.</p>
-        <p>Siga a Diocese:</p>
-        <div className="social-icons">
-          <a
-            href="https://dcl.org.br/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Site Oficial da Diocese"
-          >
-            <FontAwesomeIcon icon={faGlobe} />
-          </a>
-          
-          <a
-            href="https://www.facebook.com/diocesedecampolimpo"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Facebook da Diocese"
-          >
-            <FontAwesomeIcon icon={faFacebookF} />
-          </a>
+        <div className="footer-content">
+          <p>Conteúdo baseado no material oficial da "Novena de Natal 2025" da Diocese de Campo Limpo.</p>
+          <p>Siga a Diocese:</p>
+          <div className="social-icons">
+            <a
+              href="https://dcl.org.br/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Site Oficial da Diocese"
+            >
+              <FontAwesomeIcon icon={faGlobe} />
+            </a>
+            
+            <a
+              href="https://www.facebook.com/diocesedecampolimpo"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Facebook da Diocese"
+            >
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
 
-          <a
-            href="https://www.instagram.com/diocesedecampolimpo/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Instagram da Diocese"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
+            <a
+              href="https://www.instagram.com/diocesedecampolimpo/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram da Diocese"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
 
-          <a
-            href="https://www.youtube.com/diocesedecampolimpo"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="YouTube da Diocese"
-          >
-            <FontAwesomeIcon icon={faYoutube} />
-          </a>
+            <a
+              href="https://www.youtube.com/diocesedecampolimpo"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="YouTube da Diocese"
+            >
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
 
+          </div>
         </div>
     </footer>
     </>
